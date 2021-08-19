808efbef95978c4dded0d17ed08c87c9fac8cf5b
Ambuj Raj <thecoderguy@outlook.com>
2021-08-19 10:13:09 +0530

    initial

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+});
+
+app.get('*', (req, res) => {
+    res.send('404');
+});
+
+
+var port = process.env.PORT || 3000;
+app.listen(port, () => {
+    console.log('listening on port 3000!');
+});
+
