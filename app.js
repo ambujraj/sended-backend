b5fcff3903706b9ac02366d4cfd6743b0f6b5d53
Ambuj Raj <thecoderguy@outlook.com>
2021-09-04 11:04:21 +0530

    minor

--- app.js
+++ app.js
@@ -2,22 +2,14 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
-
+const config = require('./config');
 
 connect();
-app.get('/', (req, res) => {
-    res.send('Hello World!');
-});
-
-app.get('*', (req, res) => {
-    res.send('404');
-});
-
-
-var port = process.env.PORT || 3000;
-app.listen(port, () => {
-    console.log('listening on port 3000!');
-});
+function listen() {
+  var port = process.env.PORT || 3000;
+  app.listen(port);
+  console.log('Express app started on port ' + port);
+}
 
 
 function connect() {
