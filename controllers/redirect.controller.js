d02a942afd8aec0810d2e0bd2a9657e9eb145d8c
Ambuj Raj <thecoderguy@outlook.com>
2021-10-05 20:31:47 +0530

    lru cache

--- controllers/redirect.controller.js
+++ controllers/redirect.controller.js
@@ -1,12 +1,23 @@
 const dbFunction = require('../middlewares/dbfunction');
 const logger = require('../services/logger');
+const LRU = require('../services/cache');
 require('dotenv').config();
-
+const lruCache = new LRU(50);
 
 exports.redirect = async(req, res) => {
     const slug = req.params.slug;
-    const originalURL = await dbFunction.getOriginalURL(String(slug));
+    const cachedOriginalURL = lruCache.read(slug);
+    let originalURL;
+    if(cachedOriginalURL!=null){
+        originalURL = cachedOriginalURL;
+    }
+    else{
+        originalURL = await dbFunction.getOriginalURL(slug);
+    }
     if(originalURL!=null){
+        if(cachedOriginalURL==null){
+            lruCache.write(slug, originalURL);
+        }
         let count = await dbFunction.updateHit(String(slug));
         logger.log('info', "[redirect.controller] Slug Called("+count+" times): "+slug, {tags: 'redirect.controller,called'});
         res.redirect(originalURL);
