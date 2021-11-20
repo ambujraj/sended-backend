6e93e8fe17baa63caca494ba4bf54cc993a074bc
Ambuj Raj <thecoderguy@outlook.com>
2021-11-21 01:03:27 +0530

    added multer without temp folder

--- controllers/inputControllers.js
+++ controllers/inputControllers.js
@@ -8,47 +8,47 @@ const dbFunction = require('../middlewares/DBFunction');
 require('dotenv').config();
 
 
-exports.upload = (req, res) => {
-        if(!req.file){
-            res.status(400).json({
-                error: 'No File Uploaded'
-            });
-        }
-        else if(req.file.size==0){
-            res.status(400).json({
-                error: 'File is empty'
-            });
-        }
-        else if(req.file.size>1e+9){
-            fs.unlinkSync(req.file.path); // Empty temp folder 
-            res.status(400).json({
-                status: "Error",
-                message: "File size too large"
-            });
-        }
-        else{
-            aws.config.setPromisesDependency();
+exports.upload = (req, res, data, fileName) => {
+        // if(!req.file){
+        //     res.status(400).json({
+        //         error: 'No File Uploaded'
+        //     });
+        // }
+        // else if(req.file.size==0){
+        //     res.status(400).json({
+        //         error: 'File is empty'
+        //     });
+        // }
+        // else if(req.file.size>1e+9){
+        //     fs.unlinkSync(req.file.path); // Empty temp folder 
+        //     res.status(400).json({
+        //         status: "Error",
+        //         message: "File size too large"
+        //     });
+        // }
+        // else{
+        //     aws.config.setPromisesDependency();
             
-            const fileName = Date.now()+"_"+req.file.originalname;
+        //     const fileName = Date.now()+"_"+req.file.originalname;
             
-            var params = {
-            //ACL: 'public-read',
-            Bucket: process.env.S3_BUCKET,
-            Body: fs.createReadStream(req.file.path),
-            Key: fileName
-            };
+            // var params = {
+            // //ACL: 'public-read',
+            // Bucket: process.env.S3_BUCKET,
+            // Body: fs.createReadStream(req.file.path),
+            // Key: fileName
+            // };
         
-            s3.upload(params, (err, data) => {
-            if (err) {
-                logger.log('error', "[inputControllers] Error while trying to upload to S3 bucket: "+err, {tags: 'inputControllers,s3'});
-                res.status(408).json({
-                    status: "Error",
-                    message: 'Something is Wrong.',
-                });
-            }
+        //     s3.upload(params, (err, data) => {
+        //     if (err) {
+        //         logger.log('error', "[inputControllers] Error while trying to upload to S3 bucket: "+err, {tags: 'inputControllers,s3'});
+        //         res.status(408).json({
+        //             status: "Error",
+        //             message: 'Something is Wrong.',
+        //         });
+        //     }
         
-            if (data) {
-                fs.unlinkSync(req.file.path); // Empty temp folder 
+            // if (data) {
+                //fs.unlinkSync(req.file.path); // Empty temp folder 
                 
                 //get presigned url
                 var params = {
@@ -90,11 +90,12 @@ exports.upload = (req, res) => {
                         });
                     }
                 });
-            }
-            });
+            // }
         }
+//             });
+//         }
         
     
-}
+// }
 
 
--- routes/api.js
+++ routes/api.js
@@ -1,6 +1,10 @@
 const router = require('express').Router();
 const multer = require('multer');
+const aws = require('aws-sdk');
+const multerS3 = require('multer-s3');
 const inputController = require('../controllers/inputControllers');
+s3 = new aws.S3();
+require('dotenv').config();
 
 router.get('/', (req, res) => {
     res.status(200).json({
@@ -14,20 +18,36 @@ router.get('/upload', (req, res)=>{
     message: 'Use POST request for upload.'
   });
 });
+let filename;
+let data;
+let upload = multer({
+  storage: multerS3({
+      s3: s3,
+      bucket: process.env.S3_BUCKET,
+      key: function (req, file, cb) {
+          data=file;
+          filename = Date.now()+"_"+file.originalname;
+          cb(null, filename);
+      }
+  })
+});
 
-router.post('/upload', (req, res) => {
-var upload = multer({  dest: '/tmp/', limits: { fieldSize: 1e+9 } }).single('file');
-upload(req, res, (err) => {
-    if (err) {
-      res.status(400).json({
-        message: 'Number of Files is not Valid'
-      })
-    }
-    else{
-      inputController.upload(req, res);
-    }
-  }
-)});
+router.post('/upload', upload.array('file',1), (req, res) => {
+    inputController.upload(req, res, data, filename);
+});
+// router.post('/upload', (req, res) => {
+// var upload = multer({  dest: '/tmp/', limits: { fieldSize: 1e+9 } }).single('file');
+// upload(req, res, (err) => {
+//     if (err) {
+//       res.status(400).json({
+//         message: 'Number of Files is not Valid'
+//       })
+//     }
+//     else{
+//       inputController.upload(req, res);
+//     }
+//   }
+// )});
     
 router.route('/*')
   .get((req, res)=>{res.status(400).json({
